e262cd8ec989c3d0f40bb7bdbd765663033d3489
Владислав <vladislav@MacBook-Air-Vladislav.local>
2026-04-16 15:46:53 +0200

    fix deco argument

--- app/test_main.py
+++ app/test_main.py
@@ -2,7 +2,7 @@ import pytest
 from app.main import get_human_age
 
 
-@pytest.mark.parametrize("cat_age: int, dog_age: int, expected: int", [
+@pytest.mark.parametrize("cat_age, dog_age, expected", [
     (0, 0, [0, 0]),
     (14, 14, [0, 0]),
     (15, 15, [1, 1]),
