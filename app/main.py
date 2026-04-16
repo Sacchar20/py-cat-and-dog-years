853128b8a619a4b28e336486f741def214bf8209
Владислав <vladislav@MacBook-Air-Vladislav.local>
2026-04-16 17:39:27 +0200

    fix valid cases

--- app/main.py
+++ app/main.py
@@ -1,8 +1,13 @@
 def get_human_age(cat_age: int, dog_age: int) -> list:
-    human_cat = ((cat_age >= 15) + (cat_age >= 24)
-                 + max(0, (cat_age - 24) // 4 * (cat_age >= 24)))
+    if not isinstance(cat_age, int) or not isinstance(dog_age, int):
+        raise TypeError("Ages must be integers")
 
-    human_dog = ((dog_age >= 15) + (dog_age >= 24)
-                 + max(0, (dog_age - 24) // 5 * (dog_age >= 24)))
+    human_cat = (cat_age >= 15) + (cat_age >= 24)
+    if cat_age >= 24:
+        human_cat += (cat_age - 24) // 4
+
+    human_dog = (dog_age >= 15) + (dog_age >= 24)
+    if dog_age >= 24:
+        human_dog += (dog_age - 24) // 5
 
     return [human_cat, human_dog]
--- app/test_main.py
+++ app/test_main.py
@@ -3,6 +3,8 @@ from app.main import get_human_age
 
 
 @pytest.mark.parametrize("cat_age, dog_age, expected", [
+    (-1, -1, [0, 0]),
+    (-5, 15, [0, 1]),
     (0, 0, [0, 0]),
     (14, 14, [0, 0]),
     (15, 15, [1, 1]),
@@ -12,5 +14,19 @@ from app.main import get_human_age
     (28, 28, [3, 2]),
     (100, 100, [21, 17])
 ])
-def test_get_human_age(cat_age: int, dog_age: int, expected: list) -> None:
+def test_get_human_age_valid_cases(cat_age: int,
+                                   dog_age: int,
+                                   expected: list) -> None:
     assert get_human_age(cat_age, dog_age) == expected
+
+
+@pytest.mark.parametrize("cat_age, dog_age", [
+    ("15", 15),
+    (15, 15.5),
+    (None, 15),
+    ([24], 24),
+])
+def test_get_human_age_raises_error_on_wrong_types(cat_age: int,
+                                                   dog_age: int) -> None:
+    with pytest.raises(TypeError):
+        get_human_age(cat_age, dog_age)
